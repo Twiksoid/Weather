eee71829b1a266dfd787d8e18bfca242cb056225
Nikita Byzov <spamsmails@icloud.com>
2022-12-11 23:04:48 +0600

    Не долетело с первого раза

--- Weather/Controllers/InitViewController.swift
+++ Weather/Controllers/InitViewController.swift
@@ -6,27 +6,49 @@
 //
 
 import UIKit
+import CoreLocation
 
 class InitViewController: UINavigationController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        for i in CoreDataManager.shared.city {
+            getDataLocationFor(lat: i.lat, lot: i.lon)
+        }}
+    
+    private func getDataLocationFor(lat: CLLocationDegrees, lot: CLLocationDegrees){
+        let tempValueFromSettings = CoreDataManager.shared.settings[0].temp
+        var tempValueString: String = ""
+        if tempValueFromSettings == 0 { tempValueString = APIData.metric } else { tempValueString = APIData.imperial}
+        
+        let urlForCertainTown = APIData.urlHTTPS+"lat=\(lat)"+"&lon=\(lot)"+"&cnt=\(APIData.cnt)"+"&appid=\(APIData.apiKey)"+"&units=\(tempValueString)"+"&lang=\(APIData.lang)"
+        
+        NetworkManager().downloadData(urlString: urlForCertainTown) { answer in
+            if answer != nil {
+                CoreDataManager.shared.saveNewWeatherDatas(weatherData: answer!)
+                
+                DispatchQueue.main.async {
+                    self.makeRootController()
+                }
+                
+                // self.makeRootController()
+            } else {
+                print("нил вернулся вместо ответа")
+            }
+        }
+    }
+    
+    private func makeRootController(){
         var viewControllerForShowing: UIViewController
+        viewControllerForShowing = PageViewController()
         
         if (UserDefaults.standard.value(forKey: "onboarding") as? String) == nil {
             viewControllerForShowing = PermissionLocationController()
             pushViewController(viewControllerForShowing, animated: true)
         } else {
-            viewControllerForShowing = PageViewController()
-            pushViewController(viewControllerForShowing, animated: true)
-        }
-        
-        
-       // pushViewController(viewControllerForShowing, animated: true)
-//        view.window?.rootViewController = viewControllerForShowing
-//        view.window?.makeKeyAndVisible()
+            DispatchQueue.main.async {
+                self.pushViewController(viewControllerForShowing, animated: true)
+            }}
     }
-
 }
